56fc07d41414d5beb83e11455ef877399073dd9a
Will Stevens <wstevens@cloudops.com>
2014-01-29 11:12:59 -0500

    fixed indenting

--- server.py
+++ server.py
@@ -119,14 +119,14 @@ def index():
 			
 			with NitroAPI(host=conf.get('NETSCALER', 'host'), username=conf.get('NETSCALER', 'user'), password=conf.get('NETSCALER', 'pass'), logging=conf.getboolean('DEFAULT', 'server_debug')) as api:
 				# enable features
-		        payload = {
-		            'sessionid':api.session,
-		            'onerror':'rollback',
-		            'nsfeature': {
-		                'feature': ['LB', 'CS', 'CMP', 'CF', 'IC', 'REWRITE', 'AppFw', 'RESPONDER']
-		            }
-		        }
-		        api.request('/config/nsfeature?action=enable', payload)
+				payload = {
+					'sessionid':api.session,
+					'onerror':'rollback',
+					'nsfeature': {
+						'feature': ['LB', 'CS', 'CMP', 'CF', 'IC', 'REWRITE', 'AppFw', 'RESPONDER']
+					}
+				}
+				api.request('/config/nsfeature?action=enable', payload)
 
 				# setup the IP addresses for the VIP and the MIP
 				if conf.get('NETSCALER', 'mip') and conf.get('NETSCALER', 'vip'):
